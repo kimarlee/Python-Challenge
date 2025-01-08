32d21d3e66536ba3698fdca922c389b954b677d8
Kimarlee <kimarlee.tran@gmail.com>
2025-01-08 02:51:02 -0600

    added completed python files with new text files that have the results

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,11 +7,16 @@ import os
 
 # Files to load and output (update with correct file paths)
 file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+file_to_output = os.path.join("Analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
 total_net = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+
 # Add more variables to track other necessary financial data
 
 # Open and read the csv
@@ -19,10 +24,13 @@ with open(file_to_load) as financial_data:
     reader = csv.reader(financial_data)
 
     # Skip the header row
-    header = next(reader)
+    csv_header = next(reader)
 
     # Extract first row to avoid appending to net_change_list
-
+    first_row = next(reader)
+    total_months = total_months + 1
+    total_net = total_net + int(first_row[1])
+    prev_net = int(first_row[1])
 
     # Track the total and net change
 
@@ -31,26 +39,43 @@ with open(file_to_load) as financial_data:
     for row in reader:
 
         # Track the total
-
+        total_months = total_months + 1
+        total_net = total_net + int(row[1])
 
         # Track the net change
-
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_of_change = month_of_change + [row[0]]
 
         # Calculate the greatest increase in profits (month and amount)
-
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
 
         # Calculate the greatest decrease in losses (month and amount)
-
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
 
 
 # Calculate the average net change across the months
-
+monthly_avg_net = sum(net_change_list) / len(net_change_list)
 
 # Generate the output summary
+output = (
+    "Financial Analysis\n"
+    "------------------------\n"
+    f"Total months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${monthly_avg_net:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
 
+)
 
 # Print the output
-
+print(output)
 
 # Write the results to a text file
 with open(file_to_output, "w") as txt_file:
